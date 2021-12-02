92e290567e63a7079fdfecee417d7fd22f00c83a
Gareth Scott-Dodd <gareth.scott-dodd@cloudreach.com>
2021-12-02 15:12:38 +0000

    progress with lambda

--- script.py
+++ script.py
@@ -5,11 +5,15 @@ s3 = boto3.resource('s3')
 
 def lambda_handler(event, context):
   bucket =  'lambdaprac1289'
-  key = 'lambdaprac1289/sample_data'
-  petname= 'Meowsalot'
+  key = 'sample_data'
+  petname= 'Purrsloud'
 
   obj = s3.Object(bucket, key)
-  petname = obj.get()['Body'].read().decode('utf-8')
+  petname = obj.get()['Body'].read()
   json_data = json.loads(petname)
 
-  print(json_data)
+  for pet in "pets":
+      name=pet
+      if name == petname:
+        favouriteFoods=",".join(pet['favFoods'])
+        return(name + " Favourite Foods Are: " + favouriteFoods)
